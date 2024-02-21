9c201bd6f0423fa71cf478463404086cf39ce695
Artalis01 <74090669+Artalis01@users.noreply.github.com>
2024-02-21 09:16:28 +0000

    Added Text Input and Change the Colour of Result Text

--- pages/Word_Correction_NER.py
+++ pages/Word_Correction_NER.py
@@ -1,1 +1,8 @@
 import streamlit as st
+import re
+
+text = st.text_input('Masukkan Teks Di sini')
+result_text = re.sub('nama', ':blue[nama]', text, re.IGNORECASE )
+
+st.markdown(result_text)
+st.button('Reset')
